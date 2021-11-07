5df69a76245275012a0052732e05c9c23d10886d
alvaropms <alvaromelquiades2001@gmail.com>
2021-11-07 19:03:09 -0300

    backend master

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+const connection = require('./connection');
+
+app.get('/', (req, res) => {
+  res.send(`<h1>Hello world. Starting on port ${process.env.PORT || connection.PORT}</h1>`);
+});
+
+const NEW_CHAT_MESSAGE_EVENT = 'newMessage';
+
+var messages = [];
+
+io.on('connection', (socket) => {
+  socket.emit(NEW_CHAT_MESSAGE_EVENT, messages);
+
+    socket.on(NEW_CHAT_MESSAGE_EVENT, (msg) => {
+      messages.push(msg);
+      socket.emit(NEW_CHAT_MESSAGE_EVENT, messages);
+      socket.broadcast.emit(NEW_CHAT_MESSAGE_EVENT, messages);
+    });
+});
+
+server.listen(process.env.PORT || connection.PORT, () => {
+  console.log(`listening on *:${process.env.PORT || connection.PORT}`);
+});
