e6146649b8eba3e77fed6e93435080b890b9681c
alvaropms <alvaromelquiades2001@gmail.com>
2021-11-07 13:36:34 -0300

    backend

--- frontend/pages/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const FormLogin = styled.form`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 90vh;
-`;
